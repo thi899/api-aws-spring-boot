ce9b287e4cc8d968e685a006c5b13cd79fc9c774
thi899 <thiagosimao74@gmail.com>
2025-04-15 21:55:12 -0300

    feat(): first commit with entities

---
+++ api/src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -0,0 +1,34 @@
+package com.eventostec.api.domain.coupon;
+
+import com.eventostec.api.domain.event.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "event-coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+
+    private Integer discount;
+
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
